1535d481731e62522bb7cfae1db9acc255524a83
LikeASycamore <38587345+LikeASycamore@users.noreply.github.com>
2018-12-07 18:22:18 +0800

    第一次提交

---
+++ data/jsondata.js
@@ -0,0 +1,52 @@
+var books = [{
+    "image": "http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F1228859%2F1228859_d14f18e849b34420904ead54936e440a.jpg%2F",
+    "bookName": "圣墟",
+"author": "辰东",
+"simpleIntro": "在破败中崛起，在寂灭中复苏。沧海成尘，雷电枯竭，那一缕幽雾又一次临近大地，世间的枷锁被打开了，一个全新的世界就此揭开神秘的一角……",
+"popularity": "17.87万",
+"save": "46.02%",
+    "recent": "7小时前:正文卷 第1316章 背对众生",
+    "comment": "31200"
+},
+
+{"image": "http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F2107590%2F2107590_2607eaf03b2542ba88e4b75ac96bb12c.jpg%2F",
+ "bookName": "元尊",
+"author": "天蚕土豆",
+"simpleIntro": "彼时的归途，已是一条命运倒悬的路。昔日的荣华，如白云苍狗，恐大梦一场。 少年执笔，龙蛇飞动是为一抹光芒劈开暮气沉沉之乱世，问鼎玉宇苍穹。复仇之路，与吾同行。一口玄黄真气定可吞天地日月星辰，雄视草木苍生。铁画夕照，雾霭银钩，笔走游龙冲九州。横姿天下，墨洒青山，鲸吞湖海纳百川。",
+"popularity": "11.60万",
+"save": "63.06%",
+  "recent":" 8小时前 正文 第六百六十六章 夺圣战！",
+  "comment": "21200"
+},{
+    "image": "http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F41584%2F41584_873f1d8b2f7a447a8c59f3573094db1b.jpg%2F1",
+    "bookName": "帝霸",
+"author": "厌笔萧生",
+"simpleIntro": "千万年前，李七夜栽下一株翠竹。八百万年前，李七夜养了一条鲤鱼。五百万年前，李七夜收养一个小女孩。…………………………今天，李七夜一觉醒来，翠竹修练成神灵，鲤鱼化作金龙，小女孩成为九界女帝。这是一个养成的故事，一个不死的人族小子养成了妖神、养成了仙兽、养成了女帝的故事",
+"popularity": "9.66万",
+"save":"69.97%",
+  "recent": "12小时前 第3505章 谈崩",
+  "comment": "41200"
+  }, {
+  "image": "http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F824066%2F824066_6114388004ba478c8ab3bce0064521fe.jpg%2F",
+  "bookName": "全职法师",
+  "author": "乱",
+  "simpleIntro": "一觉醒来，世界大变。熟悉的高中传授的是魔法，告诉大家要成为一名出色的魔法师。居住的都市之外游荡着袭击人类的魔物妖兽，虎视眈眈。崇尚科学的世界变成了崇尚魔法，偏偏有着一样以学渣看待自己的老师，一样目光异样的同学，一样社会底层挣扎的爸爸，一样纯美却不能走路的非血缘妹妹……不过，莫凡发现绝大多数人都只能够主修一系魔法，自己却是全系全能法师！",
+  "popularity": "9.12万",
+  "save": "65.94%",
+  "recent":"一天前 正文卷 第2612章 原始厮杀",
+  "comment": "39200"
+  }, {
+  "image": "http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F2044498%2F2044498_b1e079fc34864ade953606f4a3ad8c68.jpg%2F",
+  "bookName": "牧神记",
+  "author": "宅猪",
+  "simpleIntro": "大墟的祖训说，天黑，别出门。大墟残老村的老弱病残们从江边捡到了一个婴儿，取名秦牧，含辛茹苦将他养大。这一天夜幕降临，黑暗笼罩大墟，秦牧走出了家门……做个春风中荡漾的反派吧！瞎子对他说。秦牧的反派之路，正在崛起！",
+  "popularity": "8.37万",
+  "save": "76.07%",
+  "recent":"12小时前:正文卷 第一千一百六十六章 坦诚相待（第四更）",
+  "comment": "28000"
+  }
+
+]
+module.exports = {
+  dataList: books
+}
